c04e086e2eb109d8bd839397d1aedf3eece43421
Wade Spires <wade@Wade-MacBook.local>
2014-09-22 18:39:14 -0400

    Added tipulator.com website.

---
+++ App Store/Website/deploy_website.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+scp -r index.html form.php stylesheets images javascript ws7@trail.dreamhost.com:/home/ws7/tipulator.com
---
+++ App Store/Website/form.php
@@ -0,0 +1,22 @@
+<?php
+
+$emailmanager = 'support@tipulator.com';
+
+error_reporting(0);
+
+$email = trim($_POST['email']);
+$Ok = ereg("^([a-zA-Z0-9_\.-]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", $email);
+if ($Ok) {
+	mail($emailmanager,'New Subscriber','You have a new subscriber!','From: '.$email);
+
+	if( !ereg("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$",$UNameFrm) )
+	{
+		echo 1;
+	}
+} 
+
+	else 
+	{
+		echo 0;	
+	}
+?>
