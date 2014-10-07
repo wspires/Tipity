cf865463d9698a16867995fa3d04c9bcbc8f9bf9
Wade Spires <wade@Wade-MacBook.local>
2014-10-07 14:37:33 -0400

    Updated website with Tipity name

--- App Store/Website/deploy_website.sh
+++ App Store/Website/deploy_website.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-scp -r index.html form.php stylesheets images javascript ws7@trail.dreamhost.com:/home/ws7/tipulator.com
+#scp -r index.html form.php stylesheets images javascript ws7@trail.dreamhost.com:/home/ws7/tipulator.com
+scp -r index.html form.php stylesheets images javascript ws7@trail.dreamhost.com:/home/ws7/tipityapp.com
