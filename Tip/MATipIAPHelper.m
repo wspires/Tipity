65b2c210188173c1999645bc0ff4dba731b26762
Wade Spires <wade@Wade-MacBook.local>
2015-03-26 17:55:23 -0400

    * Disabled IAP for Watch release (convert to paid in iTunes). * Added Watch screenshots.

--- Tip/MATipIAPHelper.m
+++ Tip/MATipIAPHelper.m
@@ -16,7 +16,7 @@
 
 // Uncomment to unlock all IAP.
 // Comment before deploying or else all IAP will be unlocked for free!
-//#define UNLOCK_ALL_IAP
+#define UNLOCK_ALL_IAP
 
 static NSUInteger const ProductCountThreshold = 3;
 
